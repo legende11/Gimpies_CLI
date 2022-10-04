abd8243ff7fe29e17f28fe2ecd78fd79bcb657d9
Thijmen Rierink <pubgtjes@gmail.com>
2022-10-04 12:10:43 +0200

    Optimised imports

--- Gimpies-form/Manager_Data.cs
+++ Gimpies-form/Manager_Data.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using Gimpies;
-
+﻿using Gimpies;
 namespace Gimpies_form
 {
     public partial class Manager_Data : Form
